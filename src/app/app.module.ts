be964008c4b4b381c7618ee81e1500896ddd89f1
Diana Montano <dianaerika.montano16@gmail.com>
2021-11-05 20:53:50 -0400

    Desktop-1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,35 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './componentes/nav-bar/nav-bar.component';
+import { HomeComponent } from './componentes/home/home.component';
+import { ImagenesComponent } from './componentes/imagenes/imagenes.component';
+import { ContactoComponent } from './componentes/contacto/contacto.component';
+import { AppRoutingModule } from './app-routing.module';
+import { CarruselComponent } from './componentes/carrusel/carrusel.component';
+import { BannerPrincipalComponent } from './componentes/banner-principal/banner-principal.component';
+import { CarouselNoticiasComponent } from './componentes/carousel-noticias/carousel-noticias.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    HomeComponent,
+    ImagenesComponent,
+    ContactoComponent,
+    CarruselComponent,
+    BannerPrincipalComponent,
+    CarouselNoticiasComponent,
+    // SidebarComponent,
+    // HeaderComponent,
+    // CardComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
---
+++ src/app/componentes/carousel-noticias/carousel-noticias.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-carousel-noticias',
+  templateUrl: './carousel-noticias.component.html',
+  styleUrls: ['./carousel-noticias.component.css']
+})
+export class CarouselNoticiasComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
